7fc26e7889515d67d0b38a077e6a2d946620e66b
Salih <mustafahakan9215@gmail.com>
2025-02-02 01:46:22 +0300

    added sections to panels. element scrolling will be fixed next

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main()
 	Vector2 panelPos5= {28, 0};
 	Vector2 panelPos6= {28, 26};
 
-	Vector2 panelSize= {10, 52};
+	Vector2 panelSize= {20, 52};
 	Vector2 panelSize2= {6, 52};
 	Vector2 panelSize3= {12, 38};
 	Vector2 panelSize4= {12, 14};
@@ -57,7 +57,7 @@ int main()
 
 	SwanGui swanGui;
 
-	auto panel1= std::make_shared<Panel>("BUTTONS AND SLIDERS", panelPos1, panelSize, custom_font);
+	auto panel1= std::make_shared<Panel>("BUTTONS AND SLIDERS", panelPos1, panelSize, 2, custom_font);
 	auto panel2= std::make_shared<Panel>("COLORS", panelPos2, panelSize2, custom_font);
 	auto panel3= std::make_shared<Panel>("BILLBOARDS", panelPos3, panelSize3, custom_font);
 	auto panel4= std::make_shared<Panel>("GIF BILLBOARD", panelPos4, panelSize4, custom_font);
@@ -105,7 +105,6 @@ int main()
 	panel1->addElement(std::make_shared<CheckBox>("Until Failure", yes));
 	panel1->addElement(std::make_shared<Button>("Restore Scene", [](){}, false));
 	panel1->addElement(std::make_shared<Button>("SAVE AND EXIT", [](){}, true));
-
 	std::string testaaa= "hello world";
 	panel1->addElement(std::make_shared<InputBox>("test", testaaa, 12));
 
